578d23b136251cee17054e9834159fb20fa27136
Dustin Lacewell <dlacewell@gmail.com>
2019-12-30 14:51:49 -0600

    Add a basic shell.nix file

---
+++ shell.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "storybro";
+  buildInputs = [
+    readline
+    which
+    gcc
+    binutils
+    openssl
+    libxml2
+    libxslt
+    aria2
+    python36Packages.poetry
+  ];
+  src = null;
+  PIPENV_VENV_IN_PROJECT=1;
+
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Augment the dynamic linker path
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${R}/lib/R/lib:${readline}/lib
+  '';
+}
